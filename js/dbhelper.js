6387189db4e98ac225ce70761607f3dd17ec2534
Zita Balint <zitag4@gmail.com>
2018-11-26 16:36:42 +0100

    Change Database URL

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -1,3 +1,4 @@
+
 /**
  * Common database helper functions.
  */
@@ -8,8 +9,9 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 8000 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+  //  const port = 8000 // Change this to your server port
+  //  return `http://localhost:${port}/data/restaurants.json`;
+  return "./data/restaurants.json";
   }
 
   /**
